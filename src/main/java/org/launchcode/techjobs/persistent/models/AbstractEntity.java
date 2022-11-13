09bd922f6e37276bace78ac088dbc5de36310965
Torrie Real <torijames1997@gmail.com>
2022-11-13 07:09:57 -0600

    -m "added @MappedSuperclass for inheritance, made subclasses entities via annotations, and made it s that user must enter name and set name length possible"

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,23 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.Objects;
 
 
+//1- give AbstractEntity the @MappedSuperclassannotation.
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    //2- Since all of the subclasses of AbstractEntity will be entities themselves, add the @Idand @GeneratedValue annotations to the field id.
+    @Id
+    @GeneratedValue
     private int id;
 
+    //3- a user cannot leave this field blank when creating an object and that the name length is a decent length
+    @NotBlank(message = "Must enter name")
+    @Size(min=3, max=100)
     private String name;
 
     public int getId() {
